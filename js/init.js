f630e7acabec772ed81b8f6e8641eff4aad9a877
Leon Anavi <leon@anavi.org>
2023-05-06 00:37:32 +0300

    index.html: TuxCon 2023
    Initial version of the website for TuxCon on 10-11 June 2023.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- js/init.js
+++ js/init.js
@@ -97,7 +97,7 @@ jQuery(document).ready(function($) {
 /*----------------------------------------------------*/
 /* Final Countdown Settings
 ------------------------------------------------------ */
-	var finalDate = '2022/06/04';
+	var finalDate = '2023/06/10';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
